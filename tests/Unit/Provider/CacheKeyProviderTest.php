74ba6b7863549d71e0614e41e29d604338beb34d
patrykbaszak <patryk.baszak@gmail.com>
2023-06-02 16:51:53 +0200

    feat(39): Added Dynamic Key Part to the Cache Key Provider and as Interface

--- tests/Unit/Provider/CacheKeyProviderTest.php
+++ tests/Unit/Provider/CacheKeyProviderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PBaszak\MessengerCacheBundle\Tests\Unit\Provider;
 
+use PBaszak\MessengerCacheBundle\Contract\Optional\DynamicKeyPart;
 use PBaszak\MessengerCacheBundle\Contract\Optional\UniqueHash;
 use PBaszak\MessengerCacheBundle\Contract\Required\Cacheable;
 use PBaszak\MessengerCacheBundle\Provider\CacheKeyProvider;
@@ -44,5 +45,32 @@ class CacheKeyProviderTest extends TestCase
         };
 
         $this->assertEquals($provider->createKey($message), $provider->createKey($message, [new ForceCacheRefreshStamp()]));
+    }
+
+    /** @test */
+    public function shouldReturnKeyWithDynamicKeyPart(): void
+    {
+        $provider = new CacheKeyProvider();
+        $length = rand(1, 100);
+        $dynamicKeyPart = uniqid();
+
+        $message = new class($length, $dynamicKeyPart) extends GetString implements Cacheable, UniqueHash, DynamicKeyPart {
+            public function __construct(int $length, private string $dynamicKeyPart)
+            {
+                parent::__construct($length);
+            }
+
+            public function getUniqueHash(): string
+            {
+                return (string) $this->length;
+            }
+
+            public function getDynamicKeyPart(): string
+            {
+                return $this->dynamicKeyPart;
+            }
+        };
+
+        $this->assertTrue(str_contains($provider->createKey($message), $dynamicKeyPart));
     }
 }
